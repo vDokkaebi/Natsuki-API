551990e6bae1718bcd85b82bb2bbaaf33950c60b
Rick <wrightricky64@gmail.com>
2021-10-07 08:35:28 +0100

    Add base ROBLOX api class

---
+++ ROBLOX/BloxlinkUserRequestJson.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace Natsuki.API.ROBLOX
+{
+    internal struct BloxlinkUserRequestJson
+    {
+        public class Root
+        {
+            public string Status;
+            public string PrimaryAccount;
+            public object? MatchingAccount;
+        }
+
+    }
+}
---
+++ ROBLOX/RoVerUserRequestJson.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace Natsuki.API.ROBLOX
+{
+    internal struct RoVerUserRequestJson
+    {
+        public class Root
+        {
+            public string Status { get; set; }
+            public string RobloxUsername { get; set; }
+            public int RobloxId { get; set; }
+        }
+    }
+}
